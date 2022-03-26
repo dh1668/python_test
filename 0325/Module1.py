cb82ac43819a7a7e2f7504b3bbb37e7565657868
dh1668 <102003945+dh1668@users.noreply.github.com>
2022-03-26 14:05:40 +0900

    Add files via upload

---
+++ 0325/Module1.py
@@ -0,0 +1,12 @@
+##함수선언부분#
+def func1() :
+    print("Module1.py의 func1()")
+
+def func2() :
+    print("Module1.py의 func2()")
+
+def func3() :
+    print("Module1.py의 func3()")
+    
+
+PI = 3.14
---
+++ 0325/Module2.py
@@ -0,0 +1,6 @@
+def func1():
+    print('Module2 --> func1()')
+
+if __name__ == '__main__':
+#test
+func1()
